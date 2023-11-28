287d66265a23f00e7e2c5bc509e812378bd5c3bb
Osama Debsa <osamas1616@gmail.com>
2023-11-28 21:27:35 +0200

    add custom book item to book details view

--- lib/features/home/presentation/view/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/view/widgets/book_details_view_body.dart
@@ -1,16 +1,22 @@
 import 'package:bookly_app/features/home/presentation/view/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly_app/features/home/presentation/view/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
+    var width = MediaQuery.of(context).size.width;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-          CustomAppBarBookDetails(),
+          const CustomAppBarBookDetails(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .15),
+            child: const FeaturedListViewItem(),
+          )
         ],
       ),
     );
--- lib/features/home/presentation/view/widgets/featured_list_view.dart
+++ lib/features/home/presentation/view/widgets/featured_list_view.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_app/features/home/presentation/view/widgets/custom_list_view_item.dart';
+import 'package:bookly_app/features/home/presentation/view/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
 class FeatureBooksListView extends StatelessWidget {
