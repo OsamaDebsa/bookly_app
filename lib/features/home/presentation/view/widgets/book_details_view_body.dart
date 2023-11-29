03f55f19708616175debdebee4aed8141fee501b
Osama Debsa <osamas1616@gmail.com>
2023-11-29 17:10:01 +0200

    add custom scroll view

--- lib/features/home/presentation/view/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/view/widgets/book_details_view_body.dart
@@ -11,44 +11,52 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          const CustomAppBarBookDetails(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .19),
-            child: const CustomBookItem(),
+    return CustomScrollView(slivers: [
+      SliverFillRemaining(
+        hasScrollBody: false,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            children: [
+              const CustomAppBarBookDetails(),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * .19),
+                child: const CustomBookItem(),
+              ),
+              const SizedBox(height: 40),
+              Text("Osama Debsa",
+                  style:
+                      Styles.textStyle30.copyWith(fontWeight: FontWeight.bold)),
+              const SizedBox(height: 6),
+              Opacity(
+                opacity: .7,
+                child: Text("datadata",
+                    style: Styles.textStyle18.copyWith(
+                        fontWeight: FontWeight.w500,
+                        fontStyle: FontStyle.italic)),
+              ),
+              const SizedBox(height: 18),
+              const BookRating(
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              const SizedBox(height: 33),
+              const BooksAction(),
+              const Expanded(child: SizedBox(height: 33)),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "You can also like",
+                  style:
+                      Styles.textStyle14.copyWith(fontWeight: FontWeight.w700),
+                ),
+              ),
+              const SizedBox(height: 16),
+              const SimilarBooksListView(),
+              const SizedBox(height: 22),
+            ],
           ),
-          const SizedBox(height: 40),
-          Text("Osama Debsa",
-              style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold)),
-          const SizedBox(height: 6),
-          Opacity(
-            opacity: .7,
-            child: Text("datadata",
-                style: Styles.textStyle18.copyWith(
-                    fontWeight: FontWeight.w500, fontStyle: FontStyle.italic)),
-          ),
-          const SizedBox(height: 18),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(height: 33),
-          const BooksAction(),
-          const SizedBox(height: 33),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "You can also like",
-              style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w700),
-            ),
-          ),
-          const SizedBox(height: 16),
-          const SimilarBooksListView(),
-        ],
-      ),
-    );
+        ),
+      )
+    ]);
   }
 }
