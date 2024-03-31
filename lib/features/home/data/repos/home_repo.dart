e187fac44c4b20558df98d1f974a5f3bf83509bc
OsamaDebsa <osamas1616@gmail.com>
2024-03-31 18:59:58 +0200

    create home repo and failure file

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+// I use the dartz package to handle multiple return values.
+// because I have two cases: either success case  or failure case.
+// Dartz provides a class called Either for this purpose.
+// Either<return type in failure case , return type in success case>
+// because I have several types of errors i will make Class or model for error and put it in core file to use in anywhere in App.
+// and  i will use it in case return type in failure case.
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<List<BookModel>> fetchFeaturedBooks();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -16,7 +15,7 @@ class BooklyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        // textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
     );
   }
